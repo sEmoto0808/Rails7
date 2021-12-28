7fea0e82c7eb48ece912e8921c1ae9773f0b807b
Sho Emoto <dd2d00seg1@gmail.com>
2021-12-28 20:07:29 +0900

    migration実施
    == 20211228105804 CreateArticles: migrating ===================================
    -- create_table(:articles)
       -> 0.0007s
    == 20211228105804 CreateArticles: migrated (0.0007s) ==========================

--- db/migrate/20211228105804_create_articles.rb
+++ db/migrate/20211228105804_create_articles.rb
@@ -1,9 +1,12 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
+      # デフォルトで :id カラムを主キー（オートインクリメント）
       t.string :title
       t.text :body
 
+      # timestampsメソッドを呼び出し
+      # :created_at, :updated_at カラムを追加、railsによって自動管理
       t.timestamps
     end
   end
