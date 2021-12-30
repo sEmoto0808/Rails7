04053226d55eb681f65a7f35fb665c0e3a3a4186
Sho Emoto <dd2d00seg1@gmail.com>
2021-12-30 21:24:40 +0900

    記事を1件作成する

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,4 +11,37 @@ class ArticlesController < ApplicationController
     # [params]はコントローラのアクションでもアクセスできる
     @article = Article.find(params[:id])
   end
+
+  # 記事を1件作成する（データベースには保存しない）
+  def new
+    # インスタンス化された記事は、ビューでフォームをビルドする時に使う
+    @article = Article.new
+  end
+
+  # 記事を1件作成する（データベースには保存する）
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      # 記事の保存に成功すると、その記事のページにリダイレクトする
+      # http://localhost:3000/articles/#{@article.id}
+      redirect_to @article
+
+      # redirect_toメソッドを使うとブラウザで新しいリクエストが発生しますが、
+      # renderメソッドは指定のビューを現在のリクエストとしてレンダリングします。
+      # ここで重要なのは、redirect_toメソッドはデータベースやアプリケーションのステートが変更された後で使うべきであるという点です。
+      # それ以外の場合に使うと、ユーザーがブラウザをリロードしたときに同じリクエストが再送信され、変更が重複してしまいます。
+    else
+      # 記事の保存に失敗した場合は、app/views/articles/new.html.erbに戻ってフォームを再表示し、
+      # Turboで問題なく動くようにステータスコード4XX（400~499のいずれか）を返す。
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # Strong Parametersを使う（強く型付けする）
+  private
+    def article_params
+      # paramsをフィルターする
+      params.require(:article).permit(:title, :body)
+    end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,12 @@
 class Article < ApplicationRecord
+  # バリデーション
+  # titleの値が必ず存在しなければならない
+  validates :title, presence: true
+  # bodyの値が存在しなければならない
+  # bodyの値には文字が10個以上含まれていなければならない
+  validates :body, presence: true, length: { minimum: 10 }
+
+  # NOTE:
+  # Active Recordは、テーブルのあらゆるカラムごとにモデル属性を自動的に定義するので、
+  # モデルファイル内でこれらの属性を宣言する必要はありません。
 end
